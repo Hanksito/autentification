1b2830aa3dc7ad4b2dc7e3226eb1e1873ec27c61
Hanksito <barjabalseiro@gmail.com>
2022-08-05 18:28:41 +0000

    front casi listo

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,26 +1,58 @@
 import React, { useContext } from "react";
 import { Context } from "../store/appContext";
-import rigoImageUrl from "../../img/rigo-baby.jpg";
+import { Link, link } from "react-router-dom";
 import "../../styles/home.css";
 
 export const Home = () => {
-	const { store, actions } = useContext(Context);
+  const { store, actions } = useContext(Context);
 
-	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!!</h1>
-			<p>
-				<img src={rigoImageUrl} />
-			</p>
-			<div className="alert alert-info">
-				{store.message || "Loading message from the backend (make sure your python backend is running)..."}
-			</div>
-			<p>
-				This boilerplate comes with lots of documentation:{" "}
-				<a href="https://github.com/4GeeksAcademy/react-flask-hello/tree/95e0540bd1422249c3004f149825285118594325/docs">
-					Read documentation
-				</a>
-			</p>
-		</div>
-	);
+  return (
+    <div className="container h-80">
+      <div className="row align-items-center h-100">
+        <div className="col-3 mx-auto">
+          <div className="text-center">
+            <img
+              id="profile-img"
+              className="rounded-circle profile-img-card"
+              src="https://i.imgur.com/6b6psnA.png"
+            />
+            <p id="profile-name" className="profile-name-card"></p>
+            <form className="form-signin">
+              <input
+                type="userName"
+                name="UserName"
+                className="form-control form-group"
+                placeholder="Username"
+                required
+              />
+              <input
+                type="password"
+                name="password"
+                className="form-control form-group"
+                placeholder="password"
+                required
+              />
+              <Link to="/single">
+                <button
+                  className="btn btn-lg btn-primary btn-block btn-signin"
+                  type="submit"
+                >
+                  enter
+                </button>
+              </Link>
+              <Link to="/demo">
+                <button
+                  type="Register"
+                  name="Register"
+                  className="btn btn-lg btn-warning btn-block btn-signin my-2"
+                >
+                  Register
+                </button>
+              </Link>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
--- src/front/js/pages/single.js
+++ src/front/js/pages/single.js
@@ -3,26 +3,150 @@ import PropTypes from "prop-types";
 import { Link, useParams } from "react-router-dom";
 import { Context } from "../store/appContext";
 import rigoImageUrl from "../../img/rigo-baby.jpg";
+import "../../styles/single.css";
 
-export const Single = props => {
-	const { store, actions } = useContext(Context);
-	const params = useParams();
+export const Single = (props) => {
+  const { store, actions } = useContext(Context);
+  const params = useParams();
 
-	return (
-		<div className="jumbotron">
-			<h1 className="display-4">This will show the demo element: {store.demo[params.theid].title}</h1>
-			<img src={rigoImageUrl} />
-			<hr className="my-4" />
-
-			<Link to="/">
-				<span className="btn btn-primary btn-lg" href="#" role="button">
-					Back home
-				</span>
-			</Link>
-		</div>
-	);
+  return (
+    <div className="container ">
+      <div className="row">
+        <ul>
+          <div className="col-lg-3 col-md-3 col-sm-12 col-xs-12 Services-tab  item">
+            <div className="folded-corner service_tab_1">
+              <div className="text">
+                <i className="fa fa-image fa-5x fa-icon-image"></i>
+                <p className="item-title">
+                  <h3> Designing</h3>
+                </p>
+                <p>
+                  This is an amazing set of animated accordions based completely
+                  on CSS. They come oriented both vertically and horizontally in
+                  order to fit properly in your project. In order to see the
+                  slides,
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="col-lg-3 col-md-3 col-sm-12 col-xs-12 Services-tab item">
+            <div className="folded-corner service_tab_1">
+              <div className="text">
+                <i className="fa fa-lightbulb-o fa-5x fa-icon-image"></i>
+                <p className="item-title">
+                  <h3> Developing</h3>
+                </p>
+                <p>
+                  This is an amazing set of animated accordions based completely
+                  on CSS. They come oriented both vertically and horizontally in
+                  order to fit properly in your project. In order to see the
+                  slides,
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="col-lg-3 col-md-3 col-sm-12 col-xs-12 Services-tab item">
+            <div className="folded-corner service_tab_1">
+              <div className="text">
+                <i className="fa fa-truck fa-5x fa-icon-image"></i>
+                <p className="item-title">
+                  <h3> Marketing</h3>
+                </p>
+                <p>
+                  This is an amazing set of animated accordions based completely
+                  on CSS. They come oriented both vertically and horizontally in
+                  order to fit properly in your project. In order to see the
+                  slides,
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="col-lg-3 col-md-3 col-sm-12 col-xs-12 Services-tab item">
+            <div className="folded-corner service_tab_1">
+              <div className="text">
+                <i className="fa fa-diamond fa-5x fa-icon-image"></i>
+                <p className="item-title">
+                  <h3> Branding</h3>
+                </p>
+                <p>
+                  This is an amazing set of animated accordions based completely
+                  on CSS. They come oriented both vertically and horizontally in
+                  order to fit properly in your project. In order to see the
+                  slides,
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="col-lg-3 col-md-3 col-sm-12 col-xs-12 Services-tab item">
+            <div className="folded-corner service_tab_1">
+              <div className="text">
+                <i className="fa fa-line-chart fa-5x fa-icon-image"></i>
+                <p className="item-title">
+                  <h3>Analytics</h3>
+                </p>
+                <p>
+                  This is an amazing set of animated accordions based completely
+                  on CSS. They come oriented both vertically and horizontally in
+                  order to fit properly in your project. In order to see the
+                  slides,
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="col-lg-3 col-md-3 col-sm-12 col-xs-12 Services-tab item">
+            <div className="folded-corner service_tab_1">
+              <div className="text">
+                <i className="fa fa-mobile fa-5x fa-icon-image"></i>
+                <p className="item-title">
+                  <h3>Mobil Apps</h3>
+                </p>
+                <p>
+                  This is an amazing set of animated accordions based completely
+                  on CSS. They come oriented both vertically and horizontally in
+                  order to fit properly in your project. In order to see the
+                  slides,
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="col-lg-3 col-md-3 col-sm-12 col-xs-12 Services-tab item">
+            <div className="folded-corner service_tab_1">
+              <div className="text">
+                <i className="fa fa-money fa-5x fa-icon-image"></i>
+                <p className="item-title">
+                  <h3> e-commerce</h3>
+                </p>
+                <p>
+                  This is an amazing set of animated accordions based completely
+                  on CSS. They come oriented both vertically and horizontally in
+                  order to fit properly in your project. In order to see the
+                  slides,
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="col-lg-3 col-md-3 col-sm-12 col-xs-12 Services-tab item">
+            <div className="folded-corner service_tab_1">
+              <div className="text">
+                <i className="fa fa-bullhorn fa-5x fa-icon-image"></i>
+                <p className="item-title">
+                  <h3> Support</h3>
+                </p>
+                <p>
+                  This is an amazing set of animated accordions based completely
+                  on CSS. They come oriented both vertically and horizontally in
+                  order to fit properly in your project. In order to see the
+                  slides,
+                </p>
+              </div>
+            </div>
+          </div>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 Single.propTypes = {
-	match: PropTypes.object
+  match: PropTypes.object,
 };
